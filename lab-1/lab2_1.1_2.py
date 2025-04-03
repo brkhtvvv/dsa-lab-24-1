4c71553a489196a95161c604016088c80fb1b1ce
Ольга Бархатова <barkhatova.olya@mail.ru>
2025-04-03 17:43:52 +0700

    выполнение задания 1.1, 2 пункт

---
+++ lab-1/lab2_1.1_2.py
@@ -0,0 +1,11 @@
+num1 = float(input('Введите первое число: '))
+num2 = float(input('Введите второе число: '))
+num3 = float(input('Введите третье число: '))
+
+numbers = []
+if 1 <= num1 <= 50:
+    print(num1)
+if 1 <= num2 <= 50:
+    print(num2)
+if 1 <= num3 <= 50:
+    print(num3)
